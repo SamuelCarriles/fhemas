5f1164662b6310b760788259c0b1e753036574d9
Samuel Carriles <samuelcarrilesbanos@gmail.com>
2026-08-11 17:54:10 -0400

    doc(index): add docstrings to each fn in fhemas.index

--- src/fhemas/index.clj
+++ src/fhemas/index.clj
@@ -3,17 +3,24 @@
    [fhemas.validator-definition.field.core :refer [get-value]]
    [fhemas.error :as error]))
 
-(defn submap? [m1 m2]
+(defn submap?
+  "Returns true when m1 is a submap of m2. Returns false when either m1 or m2 is not a map."
+  [m1 m2]
   (and (map? m1)
        (map? m2)
        (= m1 (select-keys m2 (keys m1)))))
 
-(defn valid? [conds resource]
+(defn valid?
+  "Returns true when the resource matches any of the conditions (OR logic).
+  Returns true when conds is empty or nil."
+  [conds resource]
   (if (seq conds)
     (boolean (some #(submap? % resource) conds))
     true))
 
 (defn create
+  "Creates an index entry from a resource. Returns nil when the resource doesn't
+  match the :when conditions or when the key/value extraction fails."
   [idx resource]
   (let [{k-path :key v-path :value n :name w :when} idx
         ->value #(-> % (get-value resource) :value)
@@ -26,7 +33,9 @@
       (valid? w resource) {n {k v}}
       :else nil)))
 
-(defmulti insert (fn [_idx _page relation] relation))
+(defmulti insert
+  "Inserts an index entry into a page according to its relation."
+  (fn [_idx _page relation] relation))
 
 (defmethod insert :default
   [_idx _page relation]
@@ -59,6 +68,7 @@
     (update-in page [n k] (fnil conj []) v)))
 
 (defn apply-indexes
+  "Applies all indexes to a single resource, merging the results into init-page."
   [indexes init-page resource]
   (reduce
    (fn [page idx-data]
@@ -69,6 +79,7 @@
    indexes))
 
 (defn build
+  "Builds the complete index page from all resources."
   [indexes resources]
   (reduce #(apply-indexes indexes %1 %2) {} resources))
 
